8415b9779e2b1f4869a5c1a6b22d8af69d53e509
楊博名 <AndrewHowCool@yangbomingde-MacBook-Air.local>
2016-12-27 15:45:15 +0800

    Initial Commit

---
+++ quick sort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  quick sort
+//
+//  Created by 楊博名 on 2016/12/27.
+//  Copyright © 2016年 楊博名. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
